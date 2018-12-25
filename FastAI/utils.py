3229ee75c0975bb6ca493f1f3d4f8c6b40b7b3dd
Oleksandr Fialko <oleksandrfialko@eds30887505.station>
2018-12-25 13:52:08 +1300

    Adding utils

---
+++ FastAI/utils.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import forest
+from sklearn.tree import export_graphviz
+import graphviz
+
+def add_datepart(df, fldname, drop=True, time=False):
+    fld = df[fldname]
+    fld_dtype = fld.dtype
+    if isinstance(fld_dtype, pd.core.dtypes.dtypes.DatetimeTZDtype):
+        fld_dtype = np.datetime64
+
+    if not np.issubdtype(fld_dtype, np.datetime64):
+        df[fldname] = fld = pd.to_datetime(fld, infer_datetime_format=True)
+    targ_pre = re.sub('[Dd]ate$', '', fldname)
+    attr = ['Year', 'Month', 'Week', 'Day', 'Dayofweek', 'Dayofyear',
+            'Is_month_end', 'Is_month_start', 'Is_quarter_end', 'Is_quarter_start', 'Is_year_end', 'Is_year_start']
+    if time: attr = attr + ['Hour', 'Minute', 'Second']
+    for n in attr: df[targ_pre + n] = getattr(fld.dt, n.lower())
+    df[targ_pre + 'Elapsed'] = fld.astype(np.int64) // 10 ** 9
+    if drop: df.drop(fldname, axis=1, inplace=True)
+        
+# create categories
+def train_cats(df):
+    for n,c in df.items():
+        if pd.api.types.is_string_dtype(c): df[n] = c.astype('category').\
+            cat.as_ordered()
+
+def proc_df(df, y_fld=None, skip_flds=None, ignore_flds=None, do_scale=False, na_dict=None,
+            preproc_fn=None, max_n_cat=None, subset=None, mapper=None):
+    if not ignore_flds: ignore_flds=[]
+    if not skip_flds: skip_flds=[]
+    if subset: df = get_sample(df,subset)
+    else: df = df.copy()
+    ignored_flds = df.loc[:, ignore_flds]
+    df.drop(ignore_flds, axis=1, inplace=True)
+    if preproc_fn: preproc_fn(df)
+    if y_fld is None: y = None
+    else:
+        if not pd.api.types.is_numeric_dtype(df[y_fld]): df[y_fld] = df[y_fld].cat.codes
+        y = df[y_fld].values
+        skip_flds += [y_fld]
+    df.drop(skip_flds, axis=1, inplace=True)
+
+    if na_dict is None: na_dict = {}
+    else: na_dict = na_dict.copy()
+    na_dict_initial = na_dict.copy()
+    for n,c in df.items(): na_dict = fix_missing(df, c, n, na_dict)
+    if len(na_dict_initial.keys()) > 0:
+        df.drop([a + '_na' for a in list(set(na_dict.keys()) - set(na_dict_initial.keys()))], axis=1, inplace=True)
+    if do_scale: mapper = scale_vars(df, mapper)
+    for n,c in df.items(): numericalize(df, c, n, max_n_cat)
+    df = pd.get_dummies(df, dummy_na=True)
+    df = pd.concat([ignored_flds, df], axis=1)
+    res = [df, y, na_dict]
+    if do_scale: res = res + [mapper]
+    return res
+
+def fix_missing(df, col, name, na_dict):
+    if pd.api.types.is_numeric_dtype(col):
+        if pd.isnull(col).sum() or (name in na_dict):
+            df[name+'_na'] = pd.isnull(col)
+            filler = na_dict[name] if name in na_dict else col.median()
+            df[name] = col.fillna(filler)
+            na_dict[name] = filler
+    return na_dict
+
+def numericalize(df, col, name, max_n_cat):
+    if not pd.api.types.is_numeric_dtype(col) and ( max_n_cat is None or len(col.cat.categories)>max_n_cat):
+        df[name] = col.cat.codes+1
+        
+def get_sample(df,n):
+    idxs = sorted(np.random.permutation(len(df))[:n])
+    return df.iloc[idxs].copy()
+
+def split_vals(a,n): 
+    return a[:n].copy(), a[n:].copy()
+
+def rmse(x,y): 
+    return math.sqrt(((x-y)**2).mean())
+
+def print_score(m):
+    res = [rmse(m.predict(X_train), y_train), rmse(m.predict(X_valid), y_valid),
+                m.score(X_train, y_train), m.score(X_valid, y_valid)]
+    if hasattr(m, 'oob_score_'): res.append(m.oob_score_)
+    print(res)
+    
+# vizualise tree
+def draw_tree(t, df, size=10, ratio=0.6, precision=0):
+    s=export_graphviz(t, out_file=None, feature_names=df.columns, filled=True,
+                      special_characters=True, rotate=True, precision=precision)
+    IPython.display.display(graphviz.Source(re.sub('Tree {',
+       f'Tree {{ size={size}; ratio={ratio}', s)))
+ 
+
+def set_rf_samples(n):
+    forest._generate_sample_indices = (lambda rs, n_samples:
+           forest.check_random_state(rs).randint(0, n_samples, n))
+    
+def reset_rf_samples():
+    forest._generate_sample_indices = (lambda rs, n_samples:
+           forest.check_random_state(rs).randint(0, n_samples, n_samples))
