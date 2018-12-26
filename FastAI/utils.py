f161d5e5ad133e1693a89e773227d088e8e01374
Oleksandr Fialko <oleksandrfialko@eds30887505.station>
2018-12-26 23:41:40 +1300

    cleaning

--- FastAI/utils.py
+++ FastAI/utils.py
@@ -1,10 +1,35 @@
+from fastai.imports import *
 import pandas as pd
+import re, math
+import feather
 import numpy as np
 from sklearn.ensemble import forest
 from sklearn.tree import export_graphviz
+import matplotlib.pyplot as plt
 import graphviz
 
+def set_plot_sizes(sml, med, big):
+    plt.rc('font', size=sml)          # controls default text sizes
+    plt.rc('axes', titlesize=sml)     # fontsize of the axes title
+    plt.rc('axes', labelsize=med)     # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=sml)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=sml)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=sml)    # legend fontsize
+    plt.rc('figure', titlesize=big)   # fontsize of the figure title
+
+
 def add_datepart(df, fldname, drop=True, time=False):
+    """add_datepart converts a column of df from a datetime64 to many columns containing
+    the information from the date. This applies changes inplace.
+
+    Parameters:
+    -----------
+    df: A pandas data frame. df gain several new columns.
+    fldname: A string that is the name of the date column you wish to expand.
+        If it is not a datetime64 series, it will be converted to one with pd.to_datetime.
+    drop: If true then the original date column will be removed.
+    time: If true time features: Hour, Minute, Second will be added.
+    """
     fld = df[fldname]
     fld_dtype = fld.dtype
     if isinstance(fld_dtype, pd.core.dtypes.dtypes.DatetimeTZDtype):
@@ -22,12 +47,80 @@ def add_datepart(df, fldname, drop=True, time=False):
         
 # create categories
 def train_cats(df):
+    """Change any columns of strings in a panda's dataframe to a column of
+    categorical values. This applies the changes inplace.
+
+    Parameters:
+    -----------
+    df: A pandas dataframe. Any columns of strings will be changed to
+        categorical values.
+    """
     for n,c in df.items():
         if pd.api.types.is_string_dtype(c): df[n] = c.astype('category').\
             cat.as_ordered()
+            
+def apply_cats(df, trn):
+    """Changes any columns of strings in df into categorical variables using trn as
+    a template for the category codes.
+
+    Parameters:
+    -----------
+    df: A pandas dataframe. Any columns of strings will be changed to
+        categorical values. The category codes are determined by trn.
+
+    trn: A pandas dataframe. When creating a category for df, it looks up the
+        what the category's code were in trn and makes those the category codes
+        for df.
+    """
+    for n,c in df.items():
+        if (n in trn.columns) and (trn[n].dtype.name=='category'):
+            df[n] = pd.Categorical(c, categories=trn[n].cat.categories, ordered=True)
+
 
 def proc_df(df, y_fld=None, skip_flds=None, ignore_flds=None, do_scale=False, na_dict=None,
             preproc_fn=None, max_n_cat=None, subset=None, mapper=None):
+    """ proc_df takes a data frame df and splits off the response variable, and
+    changes the df into an entirely numeric dataframe.
+
+    Parameters:
+    -----------
+    df: The data frame you wish to process.
+
+    y_fld: The name of the response variable
+
+    skip_flds: A list of fields that dropped from df.
+
+    ignore_flds: A list of fields that are ignored during processing.
+
+    do_scale: Standardizes each column in df. Takes Boolean Values(True,False)
+
+    na_dict: a dictionary of na columns to add. Na columns are also added if there
+        are any missing values.
+
+    preproc_fn: A function that gets applied to df.
+
+    max_n_cat: The maximum number of categories to break into dummy values, instead
+        of integer codes.
+
+    subset: Takes a random subset of size subset from df.
+
+    mapper: If do_scale is set as True, the mapper variable
+        calculates the values used for scaling of variables during training time (mean and standard deviation).
+
+    Returns:
+    --------
+    [x, y, nas, mapper(optional)]:
+
+        x: x is the transformed version of df. x will not have the response variable
+            and is entirely numeric.
+
+        y: y is the response variable
+
+        nas: returns a dictionary of which nas it created, and the associated median.
+
+        mapper: A DataFrameMapper which stores the mean and standard deviation of the corresponding continuous
+        variables which is then used for scaling of during test-time.
+    """
     if not ignore_flds: ignore_flds=[]
     if not skip_flds: skip_flds=[]
     if subset: df = get_sample(df,subset)
@@ -57,6 +150,22 @@ def proc_df(df, y_fld=None, skip_flds=None, ignore_flds=None, do_scale=False, na
     return res
 
 def fix_missing(df, col, name, na_dict):
+    """ Fill missing data in a column of df with the median, and add a {name}_na column
+    which specifies if the data was missing.
+
+    Parameters:
+    -----------
+    df: The data frame that will be changed.
+
+    col: The column of data to fix by filling in missing data.
+
+    name: The name of the new filled column in df.
+
+    na_dict: A dictionary of values to create na's of and the value to insert. If
+        name is not a key of na_dict the median will fill any missing data. Also
+        if name is not a key of na_dict and there is no missing data in col, then
+        no {name}_na column is not created.
+    """
     if pd.api.types.is_numeric_dtype(col):
         if pd.isnull(col).sum() or (name in na_dict):
             df[name+'_na'] = pd.isnull(col)
@@ -66,10 +175,36 @@ def fix_missing(df, col, name, na_dict):
     return na_dict
 
 def numericalize(df, col, name, max_n_cat):
+    """ Changes the column col from a categorical type to it's integer codes.
+
+    Parameters:
+    -----------
+    df: A pandas dataframe. df[name] will be filled with the integer codes from
+        col.
+
+    col: The column you wish to change into the categories.
+    name: The column name you wish to insert into df. This column will hold the
+        integer codes.
+
+    max_n_cat: If col has more categories than max_n_cat it will not change the
+        it to its integer codes. If max_n_cat is None, then col will always be
+        converted.
+    """
     if not pd.api.types.is_numeric_dtype(col) and ( max_n_cat is None or len(col.cat.categories)>max_n_cat):
         df[name] = col.cat.codes+1
         
 def get_sample(df,n):
+    """ Gets a random sample of n rows from df, without replacement.
+
+    Parameters:
+    -----------
+    df: A pandas data frame, that you wish to sample from.
+    n: The number of rows you wish to sample.
+
+    Returns:
+    --------
+    return value: A random sample of n rows of df.
+    """
     idxs = sorted(np.random.permutation(len(df))[:n])
     return df.iloc[idxs].copy()
 
@@ -78,21 +213,20 @@ def split_vals(a,n):
 
 def rmse(x,y): 
     return math.sqrt(((x-y)**2).mean())
-
-def print_score(m):
-    res = [rmse(m.predict(X_train), y_train), rmse(m.predict(X_valid), y_valid),
-                m.score(X_train, y_train), m.score(X_valid, y_valid)]
-    if hasattr(m, 'oob_score_'): res.append(m.oob_score_)
-    print(res)
     
-# vizualise tree
 def draw_tree(t, df, size=10, ratio=0.6, precision=0):
+    """ Draws a representation of a random forest in IPython.
+
+    Parameters:
+    -----------
+    t: The tree you wish to draw
+    df: The data used to train the tree. This is used to get the names of the features.
+    """
     s=export_graphviz(t, out_file=None, feature_names=df.columns, filled=True,
                       special_characters=True, rotate=True, precision=precision)
     IPython.display.display(graphviz.Source(re.sub('Tree {',
        f'Tree {{ size={size}; ratio={ratio}', s)))
- 
-
+    
 def set_rf_samples(n):
     forest._generate_sample_indices = (lambda rs, n_samples:
            forest.check_random_state(rs).randint(0, n_samples, n))
@@ -100,3 +234,10 @@ def set_rf_samples(n):
 def reset_rf_samples():
     forest._generate_sample_indices = (lambda rs, n_samples:
            forest.check_random_state(rs).randint(0, n_samples, n_samples))
+    
+def parallel_trees(m, fn, n_jobs=8):
+        return list(ProcessPoolExecutor(n_jobs).map(fn, m.estimators_))
+    
+def rf_feat_importance(m, df):
+    return pd.DataFrame({'cols':df.columns, 'imp':m.feature_importances_}
+                       ).sort_values('imp', ascending=False)
