e66303256e97e7041375637e1a9f9354c4e6a29e
Oleksandr Fialko <Oleksandr.Fialko@qrious.co.nz>
2020-11-29 05:35:25 +0000

    adding Luna_Classifier

---
+++ Luna_Classifier/precache.py
@@ -0,0 +1,36 @@
+
+from torch.utils.data import DataLoader
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+from collections import namedtuple
+#log.info(__file__)
+
+from dsets import LunaDataset
+from utils import enumerateWithEstimate
+    
+cli_args = namedtuple('cli_args',['batch_size', 'num_workers'])
+cli_args.batch_size = 10
+cli_args.num_workers = 4
+    
+def main():
+        log.info("Starting {}, {}".format(__name__, cli_args))
+
+        prep_dl = DataLoader(
+            LunaDataset(),
+            batch_size =cli_args.batch_size,
+            num_workers=cli_args.num_workers,
+        )
+
+        batch_iter = enumerateWithEstimate(
+            prep_dl,
+            "Stuffing cache",
+            start_ndx=prep_dl.num_workers,
+        )
+        for _ in batch_iter:
+            pass
+        
+if __name__ == "__main__":
+    main()
---
+++ Luna_Classifier/training.py
@@ -0,0 +1,8 @@
+from torch.utils.data import 
+
+
+METRICS_LABEL_NDX=0
+METRICS_PRED_NDX=1
+METRICS_LOSS_NDX=2
+METRICS_SIZE = 3
+
