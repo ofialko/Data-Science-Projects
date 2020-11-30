4190529709afa984ebbc4560b565c5e8eccb8e79
Olek <oleksandrfialko@gmail.com>
2020-11-30 16:01:41 +1300

    feat: get_data

--- Luna_Classifier/get_data.py
+++ Luna_Classifier/get_data.py
@@ -8,33 +8,39 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 
-def streaming(url, output_path):
-    # Streaming, so we can iterate over the response.
-    with requests.get(url, stream=True) as response:
-        response.raise_for_status()
-        total_size_in_bytes= int(response.headers.get('content-length', 0))
-        block_size = 8192
-        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-        with open(output_path, 'wb') as file:
-            for data in response.iter_content(block_size):
-                if data:
-                    progress_bar.update(len(data))
-                    file.write(data)
-        progress_bar.close()
-        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
-            log.error("Something went wrong")
+class getData:
+    def __init__(self, url, output_dir):
+        self.url = url
+        self.file_name = self.url.split('/')[-1].split('?')[0]
+        self.output_path = Path(output_dir) / self.file_name
 
-
-def main():
-    with open('config/data_config.json','r') as fp:
-        data_conf = json.load(fp)
+    @classmethod
+    def from_json(cls, json_config):
+        with open(json_config,'r') as fp:
+            data_conf = json.load(fp)
         url = data_conf['url']
-        file_name = url.split('/')[-1].split('?')[0]
+        output_dir = data_conf['output_dir']
+        return cls(url, output_dir)
 
-    output_dir = Path(data_conf['output_dir'])
-    log.info(f"Starting reading {file_name}")
-    streaming(url, output_dir / file_name)
-    
+
+    def stream(self):
+        # Streaming, so we can iterate over the response.
+        log.info(f"Starting reading {self.file_name}")
+        with requests.get(self.url, stream=True) as response:
+            response.raise_for_status()
+            total_size_in_bytes= int(response.headers.get('content-length', 0))
+            block_size = 8192
+            progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+            with open(self.output_path, 'wb') as file:
+                for data in response.iter_content(block_size):
+                    if data:
+                        progress_bar.update(len(data))
+                        file.write(data)
+            progress_bar.close()
+            if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+                log.error("Something went wrong")        
+
 
 if __name__ == "__main__":
-    main()
+    json_config = Path('config') / 'data_config.json'
+    getData.from_json(json_config).stream()
